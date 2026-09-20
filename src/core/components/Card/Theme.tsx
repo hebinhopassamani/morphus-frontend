914513954158e5b530ddd5b8b7b303c88b5f03ae
Hebinho Passamani <hebinhopassamani@gmail.com>
2026-09-20 18:09:02 -0300

    .

--- src/core/components/Card/Theme.tsx
+++ src/core/components/Card/Theme.tsx
@@ -4,7 +4,7 @@ import { createTheme } from 'flowbite-react/helpers/create-theme';
 export const mpsInitialCardTheme = createTheme<MpsCardTheme>({
     root: {
         color: 'mps-info',
-        base: 'flex flex-col justify-start items-center flex-auto w-200 border-3 border-mps-neutral-variante-600 overflow-hidden rounded-xl shadow-gray-600 shadow-2xl',
+        base: 'flex flex-col justify-start text-md items-center flex-auto w-200 border-3 border-mps-neutral-variante-600 overflow-hidden rounded-xl shadow-gray-600 shadow-2xl',
         header: {
             color: 'mps-info-container',
             base: 'flex flex-row justify-start items-center h-18 w-full px-4 gap-4 border-b-2 border-b-mps-neutral-variante-600',
